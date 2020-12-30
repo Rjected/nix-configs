a37e02cd0d9430c9dcf594f229a575b12fa55286
Dan Cline <dan@dancline.net>
2020-12-30 15:00:06 -0500

    remove rescuetime service

--- nixpkgs/rescuetime.nix
+++ nixpkgs/rescuetime.nix
@@ -5,15 +5,4 @@
     rescuetime
   ];
 
- systemd.user.services.rescuetime-daemon = {
-   serviceConfig = {
-     Type = "forking";
-     ExecStart = "${pkgs.rescuetime}/bin/rescuetime";
-     ExecStop = "pkill rescuetime";
-     Restart = "on-failure";
-   };
-   wantedBy = [ "default.target" ];
- };
-
- systemd.user.services.rescuetime-daemon.enable = true;
 }
