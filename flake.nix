905e112f90a063d06dc7ab702222caf4ffe9bc08
Dan Cline <dan@dancline.net>
2021-06-16 11:40:52 -0400

    add home-manager to flake

--- flake.nix
+++ flake.nix
@@ -2,16 +2,35 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    home-manager.url = "github:nix-community/home-manager";
+  };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, home-manager, nixpkgs }: {
     nixosConfigurations.dan-nixos-sff = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./nixos/desktopConfiguration.nix ];
+      modules = [
+        ./nixos/desktopConfiguration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.rjected = import ./nixpkgs/home.nix;
+        }
+      ];
     };
     nixosConfigurations.dan-nixos-laptop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./nixos/laptopConfiguration.nix ];
+      modules = [
+        ./nixos/laptopConfiguration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.rjected = import ./nixpkgs/home.nix;
+        }
+      ];
     };
   };
 }
